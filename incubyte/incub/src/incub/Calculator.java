7e70efad27af01bf39d37c384e4d9347031c6244
mnk1997 <mayankraj2106@gmail.com>
2024-02-17 13:50:53 +0530

    3rd version contains some modification from 2nd version and one test case gets added

--- incubyte/incub/src/incub/Calculator.java
+++ incubyte/incub/src/incub/Calculator.java
@@ -9,13 +9,20 @@ public class Calculator {
 	@Test
 	public void CalculatorAddTest()
 	{
-	    assertEquals(0,"");
+	    assertEquals(0,addNumber(""));
+	    String testCaseTwo="1,1";
+	    assertEquals(2,addNumber(testCaseTwo));
 	   
 	}
 	
 	public int addNumber(String input)
 	{
-		return 0;
+		if(input.isEmpty())
+		     return 0;
+		String[] val=input.split(",");
+		int firstNumber=Integer.parseInt(val[0]);
+		int secondNumber=Integer.parseInt(val[1]);
+		return firstNumber+secondNumber;
 	}
 	
 
