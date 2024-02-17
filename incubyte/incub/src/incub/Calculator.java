ccab7c80b3066d351d84ed95acf4b2f61fbdbce1
mnk1997 <mayankraj2106@gmail.com>
2024-02-17 14:20:31 +0530

    5th version which suppports multiple delimeter also

--- incubyte/incub/src/incub/Calculator.java
+++ incubyte/incub/src/incub/Calculator.java
@@ -14,14 +14,19 @@ public class Calculator {
 	    assertEquals(2,addNumber(testCaseTwo));
 	    String testCaseThree="1,2,3,4";
 	    assertEquals(10,addNumber(testCaseThree));
-	   
+	    String testCaseFour="//5;6\n7,\\";  
+	    assertEquals(18,addNumber(testCaseFour));
 	}
 	
 	public int addNumber(String input)
 	{
 		if(input.isEmpty())
 		     return 0;
-		String[] val=input.split(",");
+		
+		//String[] val=input.split(",");
+		//\\ represent \ in string regex as it is an escape character . Thus I have supported delimeter like \ \\ \n ; , / //
+		String[] val=input.split("[;\\n,//\\\\]+");
+	
 		/*
 		 * int firstNumber=Integer.parseInt(val[0]); int
 		 * secondNumber=Integer.parseInt(val[1]);
@@ -29,7 +34,8 @@ public class Calculator {
 		int sum=0;
 		for(String s:val)
 		{
-			sum+=Integer.parseInt(s);
+			if(!s.isEmpty())
+			    sum+=Integer.parseInt(s);
 		}
 		return sum;
 	}
