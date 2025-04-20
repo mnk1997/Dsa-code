a9b7f5992e4b7c9e1ec8e492076dd49afc059f02
mnk1997 <72327083+mnk1997@users.noreply.github.com>
2025-04-20 13:32:48 +0530

    Add files via upload

--- dp/MaximumSumValue.java
+++ dp/MaximumSumValue.java
@@ -1,57 +1,53 @@
-package in.cdac.dp;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
-public class MaximumSumValue {
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Integer> A=new ArrayList<>(Arrays.asList(11,-32,34,11,-5,-3,-28,18,31,29));
-		int B=-28;
-		int C=-15;
-		int D=0;
-		System.out.println("Answer "+  solve(A,B,C,D));
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.println();
+        ArrayList<Integer> list = new ArrayList<>(List.of(11,-32,34,11,-5,-3,-28,18,31,29));
 
-	}
-	
-	  public static int solve(ArrayList<Integer> A, int B, int C, int D) {
+        int ans=solve(list,-28,-15,0);
+        System.out.println(ans);
+    }
 
-	       // Collections.sort(A);
-		  int[] maxLeft =new int[A.size()];
-		  maxLeft[0]=A.get(0)*B;
-		  for(int i=1;i<A.size();i++)
-		  {
-			  maxLeft[i]=Math.max(maxLeft[i-1], A.get(i)*B);
-		  }
-		  
-		  int[] maxMiddle=new int[A.size()];
-		  maxMiddle[0]=maxLeft[0];
-		  for(int i=1;i<A.size();i++)
-		  {
-			  maxMiddle[i]=Math.max(maxMiddle[i-1]+C*A.get(i-1),maxMiddle[i-1]+C*A.get(i));
-		  }
-		  
-		  int[] maxRight=new int[A.size()];
-		  maxRight[0]= maxMiddle[0];
-		  for(int i=1;i<A.size();i++)
-		  {
-			  maxRight[i]=Math.max(maxRight[i-1]+D*A.get(i-1),maxRight[i-1]+D*A.get(i));
-		  }
-	       
-	        return maxRight[A.size()-1];
-	    }
-	//this doesn't take care of i<=j
-		/*
-		 * public static int solve(ArrayList<Integer> A, int B, int C, int D) {
-		 * 
-		 * // Collections.sort(A); ArrayList<Integer> values=new ArrayList<>();
-		 * values.add(B); values.add(C); values.add(D); Collections.sort(values); int
-		 * ans=0; for(int i=0;i<values.size();i++) { int tempAns=Integer.MIN_VALUE;
-		 * for(int j=0;j<A.size();j++) { tempAns=Math.max(tempAns,
-		 * A.get(j)*values.get(i)); } System.out.println("for vlaues "+values.get(i)
-		 * +"  max possble vlaue is "+tempAns); ans+=tempAns; } return ans; }
-		 */
+    public static int solve(ArrayList<Integer> A, int B, int C, int D) {
 
+        int[][] dp=new int[4][A.size()+1];
+        int ans=0;
+        for(int i=0;i<dp.length;i++)
+        {
+            for(int j=0;j<dp[i].length;j++)
+            {
+                dp[i][j]=Integer.MIN_VALUE;
+            }
+        }
+        for(int col=1;col<dp[1].length;col++)
+        {
+            dp[1][col]=Math.max(dp[1][col-1],A.get(col-1)*B);
+        }
+        for(int row=0;row<dp.length;row++)
+        {
+            for(int col=0;col<dp[row].length;col++)
+            {
+                System.out.print(dp[row][col]+" ");
+            }
+            System.out.println();
+        }
+        int[] val={B,C,D};
+        for(int i=2;i<dp.length;i++)
+        {
+            for(int j=1;j<dp[i].length;j++)
+            {
+                dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]+(val[i-1]*A.get(j-1)));
+            }
+        }
+        ans=dp[3][A.size()];
+        return ans;
+    }
 }
